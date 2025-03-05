317e3fb8a4317203d15e94d9e78b93b4890b2615
zerock <kangckck24@gmail.com>
2025-03-05 09:26:51 +0900

    1st

--- src/components/todo/todoAdd2.tsx
+++ src/components/todo/todoAdd2.tsx
@@ -1,11 +1,28 @@
+import {useActionState} from "react";
+import {postTodo} from "../../apis/todoApi.tsx";
 
+
+async function submitAction (prevState:unknown , formData:FormData):Promise<number> {
+
+    console.log(prevState, formData)
+
+    const todoObj: Todo  = {title:'AAA', writer:'kyc'}
+
+    return postTodo(todoObj)
+}
 
 function TodoAdd2() {
+
+    const [state, formAction, isPending] = useActionState(submitAction, 0)
+
+    console.log(state, formAction, isPending)
+
+
     return (
         <div>
             <h1>Todo Add 2</h1>
             <form>
-
+                <button>SUBMIT</button>
             </form>
         </div>
     );
