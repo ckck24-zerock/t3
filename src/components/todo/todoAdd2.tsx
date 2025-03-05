d1cca869d08a23789fd41117d074cdf847437051
zerock <kangckck24@gmail.com>
2025-03-05 09:39:31 +0900

    1st

--- src/components/todo/todoAdd2.tsx
+++ src/components/todo/todoAdd2.tsx
@@ -2,7 +2,7 @@ import {useActionState} from "react";
 import {postTodo} from "../../apis/todoApi.tsx";
 
 
-async function submitAction (prevState:unknown , formData:FormData):Promise<number> {
+async function submitAction (prevState:number , formData:FormData):Promise<number> {
 
     console.log(prevState, formData)
 
@@ -13,15 +13,23 @@ async function submitAction (prevState:unknown , formData:FormData):Promise<numb
 
 function TodoAdd2() {
 
+    //처음 state 초기값
+    //state는 나중에 formAction의 리턴값
+    //isPending은 실행 중이라는 표시
     const [state, formAction, isPending] = useActionState(submitAction, 0)
 
-    console.log(state, formAction, isPending)
+    console.log("Component State : " + state)
 
 
     return (
         <div>
             <h1>Todo Add 2</h1>
-            <form>
+
+            {isPending && <h1>Pending...............</h1>}
+
+            <h1>RESULT : {state}</h1>
+
+            <form action={formAction}>
                 <button>SUBMIT</button>
             </form>
         </div>
